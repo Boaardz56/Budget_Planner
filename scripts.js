5d696c0f792ccda2204beceff44dfed1e869ca02
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-23 14:35:25 -0400

    Ajax for Countries Cities API.  Proveides us an object with list of countries and cities.

--- scripts.js
+++ scripts.js
@@ -4,26 +4,43 @@
 var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=qcE8d6F20uPnRxCHeTizvaUcp5ZzbYiCqValJs8Y";
 
 $.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-console.log (response);
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
 });
 
 
-//trails api not working cross origin ref
- var APIKey = "8c360f11femshd6bee130a2f26b1p1ca187jsn90567bb8ec6a";
-  var queryURL ="https://trailapi-trailapi.p.rapidapi.com/trails/explore/" + APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-  console.log (response);
-  });
+
+//Getting an object with a list of countries.
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://countries-cities.p.rapidapi.com/location/country/list?format=json",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "countries-cities.p.rapidapi.com",
+        "x-rapidapi-key": "eec2f665f6msh2d3f36fae53a543p11853ajsn8b422c1ada74"
+    }
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
 
 
+//Getting list of cities.
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://countries-cities.p.rapidapi.com/location/country/US/city/list?page=2&per_page=20&format=json&population=1",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "countries-cities.p.rapidapi.com",
+        "x-rapidapi-key": "eec2f665f6msh2d3f36fae53a543p11853ajsn8b422c1ada74"
+    }
+}
 
-
-  
-
-  
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
