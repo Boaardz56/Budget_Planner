478751a5895b8b74a5950d5395f5d28af5ae1212
saarahah <sarahlferguson07@gmail.com>
2020-06-23 14:01:54 -0400

    added trails api with cross origin issue

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 
-
 //working national park service AJAX call
 
 var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=qcE8d6F20uPnRxCHeTizvaUcp5ZzbYiCqValJs8Y";
@@ -10,6 +9,20 @@ $.ajax({
 }).then(function(response) {
 console.log (response);
 });
+
+
+//trails api not working cross origin ref
+ var APIKey = "8c360f11femshd6bee130a2f26b1p1ca187jsn90567bb8ec6a";
+  var queryURL ="https://trailapi-trailapi.p.rapidapi.com/trails/explore/" + APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+  console.log (response);
+  });
+
+
+
 
   
 
