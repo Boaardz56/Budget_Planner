48b46b08be9d608166c1f6883ac961ea2280671e
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-23 14:01:40 -0400

    Ignore this.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// var queryURL = "https://thefork.p.rapidapi.com/locations/list?google_place_id=ChIJ53USP0nBhkcRjQ50xhPN_zw" + apiKey
+// var apiKey = "eec2f665f6msh2d3f36fae53a543p11853ajsn8b422c1ada74"
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// })
+//     .then(function (response) {
+
+// console.log(response)
+//     })
