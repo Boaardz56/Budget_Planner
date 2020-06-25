a2c235891c42bd2b022f18d1054dac36ca6dc2ea
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-06-25 11:40:29 -0400

    Added functionality with the search value.  Takes data from text to be used.

--- scripts.js
+++ scripts.js
@@ -21,16 +21,22 @@ $.ajax({
   headers:{
     Authorization: "Bearer U3DP3tTXAE_o7T9a7hSMOS4MGwikjj-Q41FB7D8gdSNu5FaUojPMLoVRDSSD09XlrU8sGL01D9uv7oP4taznIPoCt_UU7zUnnakL0xSCyNRd7Z22JLeLQLye7E7yXnYx"
   }
-  
+
 }).then(function(response) {
 console.log ("yelp" , response);
 });
 
+//Button event for user's search.
 $("#submitBtn").click(function(){
   $("#searchField").hide();
   $("#checkBox").hide();
   $("#submitBtn").hide();
-  console.log("works")
+//Made it so the search data/text is taken and can be used.  cardHeader variable is a placeholder to demonstrate that the code is working with the page.
+  var cardHeader = $("h4");
+  var userSearch = $("#searchField").val();
+  cardHeader.text(userSearch)
+  console.log(userSearch)
+
 
 })
 
