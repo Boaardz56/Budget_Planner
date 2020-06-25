3fe04b4ff26761d71ad96334fcfc0c36695bb1d3
RichT <rrtrenchf@gmail.com>
2020-06-25 11:04:41 -0400

    button click function that removes home search bar added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function(){
 //google api to get some restaurants with rating around a location
 //google places AJAX call
 var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&rankby=prominence&fields=photos,formatted_address,name,rating&key=AIzaSyCZv8-G_j3tkOqJ5sIqhGFN0iYBDs-Q664";
@@ -34,4 +34,12 @@ $.ajax({
 console.log ("yelp" , response);
 });
 
+$("#submitBtn").click(function(){
+  $("#searchField").hide();
+  $("#checkBox").hide();
+  $("#submitBtn").hide();
+  console.log("works")
 
+})
+
+})
